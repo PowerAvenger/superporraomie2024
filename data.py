5858db925f5312e62073548df3a96f838a1ba8b7
Jspeedman <160780053+Jspeedman@users.noreply.github.com>
2024-05-27 09:06:51 +0200

    Add files via upload

---
+++ data.py
@@ -0,0 +1,15 @@
+#import reflex as rx
+import pandas as pd
+#import openpyxl
+import math
+
+ruta= r'C:\Users\jovid\Documents\015 JOSE\070 ENERGIA\00001 PUBLICACIONES LINKEDIN\002 Miniporras\SUPERPORRA2024.xlsm'
+
+datos=pd.read_excel(ruta,sheet_name='acum_porc')
+#datos=datos.iloc[:,9]
+datos=datos[['Clasificación', '%_desvio']]
+datos=datos.to_dict(orient='records')
+datos = [dato for dato in datos if not math.isnan(dato['%_desvio'])]
+for registro in datos:
+    registro["%_desvio"] = "{:.3f}".format(registro["%_desvio"])
+

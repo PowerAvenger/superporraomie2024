623ded367565649804bf874c66ee244916e403cc
Jspeedman <160780053+Jspeedman@users.noreply.github.com>
2024-05-28 09:49:13 +0200

    Update data.py

--- data.py
+++ data.py
@@ -3,8 +3,8 @@ import pandas as pd
 #import openpyxl
 import math
 
-ruta= r'C:\Users\jovid\Documents\015 JOSE\070 ENERGIA\00001 PUBLICACIONES LINKEDIN\002 Miniporras\SUPERPORRA2024.xlsm'
-
+#ruta= r'C:\Users\jovid\Documents\015 JOSE\070 ENERGIA\00001 PUBLICACIONES LINKEDIN\002 Miniporras\SUPERPORRA2024.xlsm'
+ruta= 'SUPERPORRA2024.xlsm'
 datos=pd.read_excel(ruta,sheet_name='acum_porc')
 #datos=datos.iloc[:,9]
 datos=datos[['Clasificación', '%_desvio']]
